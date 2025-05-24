bb67f9c270dec9968576932f794c97f71c90aba2
Darshan <darshannaik484@gmail.com>
2025-05-24 10:29:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+function redirectToDonate() {
+    alert("You are redirecting to Donate Blood page...");
+    setTimeout(function() {
+        window.location.href = "DonatorHome.html";
+    }, 2000); // 2-second delay
+}
+</script>
